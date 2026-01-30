80a51c86d2ad9d6e0f5abe75b6bdafac984065e9
lucybardgett <lb1239@exeter.ac.uk>
2026-01-30 10:17:53 +0000

    AgeToGroup completed with ternary operator

---
+++ AgeToGroup.java
@@ -0,0 +1,11 @@
+public class AgeToGroup {
+    public static void main(String[] args) {
+        int age = Integer.parseInt(args[0]);
+
+        int ageGroup = 1;
+            ageGroup = (age<18)?0:1;
+            ageGroup = (age>65)?2:ageGroup;       
+            
+        System.out.println("Age: " + age + ", Age Group: " + ageGroup);
+    }
+}
